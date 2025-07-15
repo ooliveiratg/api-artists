51176b135850c2aabe45f5559cde2ae17b325f0a
ooliveiratg <menezesth16@gmail.com>
2025-07-15 18:13:17 -0300

    feat(album): added an update method for albums

---
+++ src/controllers/albuns/list.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "../../models/generated/client/index.js";
+
+
+const prisma = new PrismaClient();
+
+export const ListAlbuns = async (req: Request, res: Response) => {
+    try {
+        const albuns = await prisma.album.findMany({
+            include: {
+                artist: true,
+                songs: true
+            }
+        });
+
+        return res.status(200).json(albuns);
+    } catch (error) {
+        return res.status(500).json({ message: "Erro interno do servidor", error });
+    }
+}
