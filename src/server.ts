011d67a57d9d1bf19a71ad20d6069f6276ae3815
ooliveiratg <menezesth16@gmail.com>
2025-07-17 09:26:02 -0300

    feat(api-docs):initializing documentation API

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,13 @@ import UserRouters from './routers/user.js';
 import AdminRouters from './routers/admin/admin.js';
 import ArtistRouters from './routers/artists/artist.js';
 import { authMiddleware } from './middlewares/auth.js';
-
-
+import YAML from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const server = express();
+
+
 const port = 3000;
 server.use(express.json())
 
@@ -14,6 +17,7 @@ server.use('/user', UserRouters);
 server.use('/admin', AdminRouters);
 server.use("/artist",authMiddleware, ArtistRouters);
 
+server.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 server.listen(port, () => {
     console.log(`está funcionando na porta: ${port}`)
